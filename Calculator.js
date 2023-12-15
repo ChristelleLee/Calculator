6f992be1546cfb2e59ccb9b0e79657305c4a47de
ChristelleLee <147832125+ChristelleLee@users.noreply.github.com>
2023-12-15 21:11:03 +0800

    Update Calculator.js

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,14 @@
-var additions= []
+var num = [0];
+
+function sum(num1){
+    var input = [];
+    var answer = 0;
+    return answer = num + input;
+}
+
+function sum(a, b){
+
+    return a + b;
+}
+
+console.log(sum(1,1));
