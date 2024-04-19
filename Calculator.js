1d2c2ece4bfc287324b0de19f03b84ef50d25b97
ChristelleLee <leeenwei10@gmail.com>
2024-04-19 22:23:30 +0800

    fix the operator bugs

--- Calculator.js
+++ Calculator.js
@@ -1,1 +1,76 @@
-var additions= []
+var cal = "";
+var result = "";
+var isOperatorPressed = true;
+var isDotPressed = true;
+var isEqualPressed = false;
+
+function clickBtn(num){
+    var display = $("#output").html();
+    if (isEqualPressed = true){
+        if(display == "0"){
+            display = num;
+        }
+        else{
+            display += num;
+        }
+    } else {
+        
+    }
+    cal += num;
+    $("#output").html(display);
+    isOperatorPressed = false;
+}
+
+function clickOP(OP) {
+    var display = $("#output").html();
+    if(isOperatorPressed == false){
+        display += OP;
+        operatorReplacer(OP)
+    } 
+    else {
+        display = display.substring(0, display.length-1);
+        display += OP;
+        operatorReplacer(OP);
+    } 
+    $("#output").html(display);
+    isOperatorPressed = true;
+}
+
+function operatorReplacer(OP){
+    if(isOperatorPressed){
+        cal = cal.substring(0, cal.length-1);
+    }
+    if (OP === "+"){
+        cal += "+";
+    } else if(OP === "-"){
+        cal += "-";
+    } else if(OP === "×"){
+        cal += "*";
+    } else if(OP === "÷"){
+        cal += "/";
+    }
+}
+
+function clickDot(dot){
+    var display = $("#output").html();
+    if (isDotPressed == false && isOperatorPressed == false){
+        display += dot;
+    } else {
+        display = display.substring(0, display.length-1);
+        display += dot;
+    }
+    isDotPressed = true;
+}
+
+function calc() {
+        result = eval(cal);
+        cal = result;
+        $("#output").html(result);
+        isEqualPressed = true;
+}
+
+function cleanDisplay(){
+    cal = "";
+    display = "0";
+    $("#output").html(display);
+}
